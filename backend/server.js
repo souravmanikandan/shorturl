cf6c500dea59bd444bc0f1121d747969d3db16fb
smcoderr <souravoz2018@gmail.com>
2025-02-14 20:35:51 +0530

    set basename in frontend and server default route in backend

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,13 @@ app.use('/api', urlsRouter);
 // ✅ Fix: Serve frontend only in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    
+    // Handle all frontend routes
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
+
 
 // Server Setup
 const PORT = process.env.PORT || 5000;
