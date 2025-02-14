a5be11886e203054431686ca49f73e054242e00e
smcoderr <souravoz2018@gmail.com>
2025-02-14 12:46:25 +0530

    url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='shorturl-bzs8.onrender.com'>
       <App />
     </BrowserRouter>
   </StrictMode>,
